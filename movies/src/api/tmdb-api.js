279e6527f3b6c3b7cd0d8603284d3d1491c25e63
Evan Sullivan <20093137@mail.wit.ie>
2023-10-25 20:36:32 +0100

    Updating movie page to use react query

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -23,10 +23,21 @@ export const getMovieReviews = (id) => {
     });
   };
   
-  export const getMovie = id => {
+  export const getMovie = (args) => {
+    // console.log(args)
+    const [, idPart] = args.queryKey;
+    const { id } = idPart;
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    ).then(res => res.json());
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
   };
  export const getGenres = async () => {
   return fetch(
@@ -44,10 +55,19 @@ export const getMovieReviews = (id) => {
  });
 };
   
-  export const getMovieImages = (id) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters);
-  };
+export const getMovieImages = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then( (response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+
+  })
+  .catch((error) => {
+    throw error
+ });
+};
